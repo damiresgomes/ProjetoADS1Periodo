8cc327d568072a4b0228f63d581891121584dceb
damiresgomes <damiresbrito2007@gmail.com>
2026-04-14 16:24:27 -0300

    commit final

---
+++ jogo/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,11 @@
+
+
+const scriptsInEvents = {
+
+	async Folhainicio_Event2_Act2(runtime, localVars)
+	{
+		runtime.goToLayout("instrucoes");
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
